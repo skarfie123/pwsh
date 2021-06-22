d204d9a0b3fe12f8e1995594bf8c533d2af63596
skarfie123 <rahulpai@rocketmail.com>
2021-06-22 23:12:37 +0100

    shortcuts module

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Currently migrating from [cmd](https://github.com/skarfie123/cmd)
 
 - [`git`](git.psm1) - Functions for working with git
 - [`meta`](meta.psm1) - Functions for working with this repo
+- [`shortcuts`](shortcuts.psm1) - Shortcuts for various apps and locations
---
+++ shortcuts.psm1
@@ -0,0 +1,95 @@
+﻿<#
+.SYNOPSIS
+    open chrome extensions folder
+#>
+function chromeExt {
+    fe "${env:LocalAppData}\Google\Chrome\User Data\Default\Extensions"
+}
+
+<#
+.SYNOPSIS
+    cd to Downloads folder
+#>
+function dl {
+    Set-Location $env:DOWNLOADS
+}
+
+<#
+.SYNOPSIS
+    explorer here
+#>
+function eh {
+    explorer .
+}
+
+<#
+.SYNOPSIS
+    file explorer here
+#>
+function feh {
+    fe "$PWD"
+}
+
+<#
+.SYNOPSIS
+    cd to home folder
+#>
+function home {
+    Set-Location $HOME
+}
+
+<#
+.SYNOPSIS
+    cd to GitHub folder
+#>
+function gh {
+    Set-Location $env:GITHUB
+}
+
+<#
+.SYNOPSIS
+    JupyterLab
+#>
+function jl {
+    py -m jupyterlab
+}
+
+<#
+.SYNOPSIS
+    open startup folder
+#>
+function startup {
+    fe "${env:AppData}\Microsoft\Windows\Start Menu\Programs\Startup\"
+}
+
+<#
+.SYNOPSIS
+    open taskbar folder
+#>
+function taskbar {
+    fe "${env:AppData}\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+}
+
+<#
+.SYNOPSIS
+    VS Code here
+#>
+function ch {
+    code .
+}
+
+<#
+.SYNOPSIS
+    Windows Terminal here
+#>
+function wth {
+    wt -w 0 nt -d .
+}
+
+<#
+.SYNOPSIS
+    Windows Terminal Quake here
+#>
+function wtq {
+    wt -w _quake nt -d .
+}
